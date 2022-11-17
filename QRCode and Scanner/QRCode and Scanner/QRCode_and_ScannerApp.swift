17f345e768684ed29327a18f5ce29cd62ea751ef
Kekelifenuku <113697135+Kekelifenuku@users.noreply.github.com>
2022-11-17 00:39:03 +0000

    Add files via upload

---
+++ QRCode and Scanner/QRCode and Scanner/QRCode_and_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCode_and_ScannerApp.swift
+//  QRCode and Scanner
+//
+//  Created by kekeli on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct QRCode_and_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
